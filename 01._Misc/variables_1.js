9e17f039646e1c82ec1935d37392cc6b2023d87e
masj0002 <masj0002@kea.stud.dk>
2025-08-28 10:56:12 +0200

    Created Variables

---
+++ 01._Misc/variables_1.js
@@ -0,0 +1,26 @@
+
+// Use const if possible
+// Else use let
+
+
+// node <file_name>
+
+const name = 'Oliver';
+// string literals
+
+const firstGreet = 'its nice """" day'
+const secondGreet = "it's nice day"
+const thirdGreet = `it's nice "" 
+den kan også gøre så man kan lave multi-line strings, altså sætte et mellemrum
+dat`
+
+console.log("Hello World", name);
+console.log(`Hello ${name}`, 2+2)
+
+//Datatyper (String, Boolean, Number, Null, Undefined, BigInt, Symbol, Object)
+
+const person = {
+    name: 'Jens' //key-value pair js objekt
+};
+//Javascript objekt (er forskelligt fra et normalt json objekt)
+
