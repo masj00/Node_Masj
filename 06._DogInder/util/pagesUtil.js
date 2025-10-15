63f54140d3c4ae261d00e490627c3b4d193b79b9
masj0002 <masj0002@kea.stud.dk>
2025-10-15 12:32:04 +0200

    update

--- 06._DogInder/util/pagesUtil.js
+++ 06._DogInder/util/pagesUtil.js
@@ -1,27 +1,22 @@
-//fs er indbygget i node da det er tungt skal man importere dere LEKSION 8/9?
-import fs from 'fs';
+import {constructPage, readPage} from "./templatingEngine"
 
 //vi læser filen før vi går videre i programmet (altså readFileSync) (læser den ind som en string)
 //den lægger her pga. den skal kun læses når projektet skal deployes(nu har vi den i memory)
 //const frontpage = fs.readFileSync("./public/pages/frontend/index.html", "utf-8")
 //vi prøver at lave SSR - server side rendering (så alt loades før) /fordele er mindre loadtider, skal ikke dom manipuleres da alt er lavet når vi sender, /
 //Reccouser bliver brugt på alle clienters mobiler osv hvis det client side rendere / men serveren gør det i server side rendering
-
-//har page bagefter fordi hvis det bare var matches ville man tro matches var en liste og ikke en side
-const header = readPage("./public/components/header/header.html");
-const footer = readPage("./public/components/footer/footer.html");
-
+//SEO OPTIMERING se chatgpt men det gør vi
+// //har page bagefter fordi hvis det bare var matches ville man tro matches var en liste og ikke en side
 
 const frontpage = readPage("./public/pages/frontend/index.html");
-export const frontpagePage = header + frontpage + footer;
+export const frontpagePage = constructPage(frontpage,{
+    tabTitle: "DogInder Welcome"
+});
 
 const matches = readPage("./public/pages/matches/matches.html");
-export const matchesPage = header + matches + footer;
+export const matchesPage = constructPage(matches, {
+    cssLinks: `<link rel="stylesheet" href="/pages/matches/matches.css">`
+});
 
 
-//før denne function
-//const frontpage = fs.readFileSync("./public/pages/frontend/index.html", "utf-8")
-
-function readPage(path){
-    return fs.readFileSync(path).toString();
-}
+//skal lige se hans git også på template engine
---
+++ 06._DogInder/util/templatingEngine.js
@@ -0,0 +1,26 @@
+//man behøver ikke at bruge TEMPLATEENGINGE LIBRARIES FORDI MAN SELV KAN LAVE DEM
+//SSR HAR DE 4 PUNKTER DER ER FOR DET LOAD TIME, RESSOURCES, SEO, CORS- SSR HAR IKKE CORS PROBLEMER
+
+//fs er indbygget i node da det er tungt skal man importere dere LEKSION 8/9?
+import fs from 'fs';
+
+
+    const header = readPage("./public/components/header/header.html");
+    const footer = readPage("./public/components/footer/footer.html");
+//den er mere dynamisk
+export function constructPage(pageContent, options = {}) {
+    //|| løser oppe i toppen altså oppe ved tabbene i browser
+    return header.replace('$$TAB_TITLE$$', options.tabTitle || "DogInder")
+    .replace('$$CSS_LINKS$$', options.cssLinks || '')
+    + pageContent 
+    + footer;
+}
+//SE HANS GIT
+
+
+//før denne function
+//const frontpage = fs.readFileSync("./public/pages/frontend/index.html", "utf-8")
+
+export function readPage(path){
+    return fs.readFileSync(path).toString();
+}
